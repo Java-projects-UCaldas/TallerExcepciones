2dfa16718189de1568618554be9e0112be2e9b95
lucasBmax <lucas.170171634@gmail.com>
2020-11-13 20:53:04 -0500

    prueba1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import modelos.Vehiculo;
+
+import javax.swing.JOptionPane;
+
+import control.VehiculoException;;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Vehiculo carro = new Vehiculo("buenas", 3000);
+        while(!carro.getAccidentado()){
+            try {
+                //carro.apagar();
+                carro.encender();
+                carro.acelerar(100);
+                carro.acelerar(200);
+                carro.detener(200);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+        }
+        
     }
 }
--- src/modelos/Vehiculo.java
+++ src/modelos/Vehiculo.java
@@ -17,7 +17,13 @@ public class Vehiculo {
     private boolean patina;
     private boolean accidentado;
 
+    public boolean getPatina(){
+        return this.patina;
+    }
 
+    public boolean getAccidentado(){
+        return this.accidentado;
+    }
     public Vehiculo(String llantas, int motor){
 
         this.llantas = llantas;
@@ -26,7 +32,7 @@ public class Vehiculo {
         this.velocidadPermitidaLlantas = asignarVelocidadLlantas(llantas);
         this.velocidadPermitidaMotor = asignarVelocidadMotor(motor);
         this.patina = false;
-        this.accidentado = true;
+        this.accidentado = false;
     }
 
     public void encender() throws VehiculoException {
@@ -42,22 +48,29 @@ public class Vehiculo {
         if(this.estado == 1){
             this.estado = 0;
         }
+        else if(this.estado == 0){
+            throw new VehiculoException("El vehículo ya se encuentra apagado");
+        }
         else if(this.velocidadActual > 60){
+            this.accidentado = true;
             throw new VehiculoException("El vehículo se accidentó por apagarse con una velocidad mayor a 60 Km/h");
         }
     }
 
     public void acelerar(int aceleracion) throws VehiculoException{
         if(aceleracion > this.velocidadPermitidaMotor){
+            this.accidentado = true;
             throw new VehiculoException("El Vehículo se accidentó por exceso de velocidad permitido para el motor");
         }
         else if((this.velocidadActual + aceleracion) > this.velocidadPermitidaMotor){
+            this.accidentado = true;
             throw new VehiculoException("El Vehículo se accidentó por exceso de velocidad permitido para el motor");
         }
         else if((this.velocidadActual + aceleracion) <= this.velocidadPermitidaMotor){
             this.velocidadActual += aceleracion;
         }
         else if((this.velocidadActual + aceleracion) > this.velocidadPermitidaLlantas){
+            this.patina = true;
             throw new VehiculoException("El vehículo patinó por exceso de velocidad permitida para las llantas");
         }
     }
@@ -67,9 +80,11 @@ public class Vehiculo {
                 throw new VehiculoException("El carro ya se encuentra detenido");
             }
             else if(this.velocidadActual > this.velocidadPermitidaLlantas && intensidad < this.velocidadActual){
+                this.patina = true;
                 throw new VehiculoException("El vehículo patinó por llevar una velocidad mayor a la permitida por sus llantas");
             }
             else if(this.velocidadActual < intensidad){
+                this.patina = true;
                 throw new VehiculoException("El vehículo patinó por frenar con una intensidad mayor a la velocidad actual");
             }
     }
@@ -77,12 +92,12 @@ public class Vehiculo {
     public int asignarVelocidadLlantas(String llantas){
         int velocidadAsignar = 0;
 
-        switch(llantas){
-            case "Buenas":
+        switch(llantas.toUpperCase()){
+            case "BUENAS":
                 velocidadAsignar = 110;
-            case "Bonitas":
+            case "BONITAS":
                 velocidadAsignar = 70;
-            case "Baratas":
+            case "BARATAS":
                 velocidadAsignar = 50;
         }
         return velocidadAsignar;
